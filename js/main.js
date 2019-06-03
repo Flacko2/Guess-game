fa881fc42e077fcec20dcc436629245bd387c01b
Diego Matthews <diegomatthews9@gmail.com>
2019-06-02 20:50:05 -0700

    Worked on basic components of game

---
+++ js/main.js
@@ -0,0 +1,23 @@
+window.onload = function {
+
+    let alphabet = [
+        'a', 'b', 'c',
+        'd', 'e', 'f',
+        'g', 'h', 'i',
+        'j', 'k', 'l',
+        'm', 'n', 'o',
+        'p', 'q', 'r',
+        's', 't', 'u',
+        'v', 'w', 'x',
+        'y', 'z'
+    ];
+
+    let word ;       //selected word
+    let guess ;      //Guess
+    let guesses = [ ];    //Stored guesses
+    let lives ;     //lives counter
+    let counter ;   //Count crrect guesses
+    
+    
+
+}
